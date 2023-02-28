f94a480c92f46a3e535ee2170c7dfd2171d73c47
SaltyFishe <3020953511@qq.com>
2023-02-28 20:49:09 +0800

    Initial commit

---
+++ src/GameTest.java
@@ -0,0 +1,26 @@
+package src;
+
+public class GameTest {
+    public static void main(String[] args) {
+        //player1
+        Role r1 = new Role("黄天戈",100);
+        //player2
+        Role r2 = new Role("个天皇",100);
+        //start
+        while (true){
+            //r1攻击r2
+            r1.attack(r2);
+            //判断r2剩余血量
+            if (r2.getBlood() == 0){
+                System.out.println(r1.getName() + "击败了" + r2.getName());
+                break;
+            }
+            r2.attack(r1);
+            if (r1.getBlood() == 0){
+                System.out.println(r2.getName() + "击败了" +r1.getName());
+                break;
+            }
+        }
+
+    }
+}
---
+++ src/Role.java
@@ -0,0 +1,51 @@
+package src;
+
+import java.util.Random;
+
+public class Role {
+    private String name;
+    private int blood;
+
+    public Role() {
+    }
+
+    public Role(String name, int blood) {
+        this.name = name;
+        this.blood = blood;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBlood() {
+        return blood;
+    }
+
+    public void setBlood(int blood) {
+        this.blood = blood;
+    }
+
+    //定义一个方法攻击
+    //方法的调用者去攻击参数
+    public void attack(Role role){
+        //计算造成的伤害1~20
+        Random r = new Random();
+        int hurt = r.nextInt(20)+1;
+        //挨打的人的血量
+        int remainBlood = role.getBlood() - hurt;
+        //对剩余血量进行验证，如果为负数就改为0
+        remainBlood = remainBlood < 0 ? 0 : remainBlood;
+        role.setBlood(remainBlood);
+
+
+
+        System.out.println(this.getName() + "打了"+ role.getName() +
+                "一下，造成了"+ hurt +"点伤害，"+ role.getName() +"还剩"+remainBlood+"血");
+
+    }
+}
